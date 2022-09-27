5c00c39c6bc7f46a6cb8989b87efa326cd902ebe
shadab195 <112242812+shadab195@users.noreply.github.com>
2022-09-27 13:42:55 +0530

    Delete climb (1).py

--- climb (1).py
+++
@@ -1,8 +0,0 @@
-def sta(n):
-    if n<=1:
-        return n
-    return sta(n-1)+sta(n-2)
-def count(s):
-    return sta(s+1)
-s=int(input("ENTER THE WAYS---> "))
-print("THE NUMBER OF WAYS----> ",(count(s)))
