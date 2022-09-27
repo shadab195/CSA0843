93d275d516e86e6b6f6b6f7aac900444f3f28b34
shadab195 <112242812+shadab195@users.noreply.github.com>
2022-09-27 13:44:11 +0530

    Delete grate.py

--- grate.py
+++
@@ -1,9 +0,0 @@
-a=int(input("ENTER THE FIRST NUMBER---> "))
-b=int(input("ENTER THE SECOND NUMBER---> "))
-c=int(input("ENTER THE THIRD NUMBER---> "))
-if(a>b)and(a>c):
-    print(a,"NUMBER IS GREATER")
-elif(b>a)and(b>c):
-    print(b,"NUMBER IS GREATER")
-else:
-    print(c,"NUMBER IS GREATER")
