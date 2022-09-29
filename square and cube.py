f2daa10b3f92498c0d1595b406e75408244ec69c
shadab195 <112242812+shadab195@users.noreply.github.com>
2022-09-29 14:06:41 +0530

    Add files via upload

---
+++ square and cube.py
@@ -0,0 +1,7 @@
+def square(num):
+     return num*num
+def cube(num):
+     return num*num*num
+number=float (input("enter the decimal:"))
+print("Square of number:", square(number))
+print("Cube of number:", cube(number))
